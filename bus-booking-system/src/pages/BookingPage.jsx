41fe85459eed59e0378967fbd12ff1a918ae0a2a
it22591302 <sandeepkahawatta9@gmail.com>
2024-07-12 13:32:46 +0530

    booking headere

--- bus-booking-system/src/pages/BookingPage.jsx
+++ bus-booking-system/src/pages/BookingPage.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import BookingForm from '../components/BookingForm'
+import Header from '../components/Header'
 
 function BookingPage() {
   return (
+    <>
+      <Header />
     <div>
         <BookingForm></BookingForm>
     </div>
+    </>
   )
 }
 
