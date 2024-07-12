e2950d9cbc5bfb1e44858bc834f41ec34dc9a962
it22591302 <sandeepkahawatta9@gmail.com>
2024-07-12 18:24:28 +0530

    add footer to booking page

--- bus-booking-system/src/pages/BookingPage.jsx
+++ bus-booking-system/src/pages/BookingPage.jsx
@@ -3,19 +3,23 @@ import BookingForm from '../components/BookingForm'
 import Header from '../components/Header'
 import AvailableBus from '../components/AvailableBus'
 import LiveBusTracker from '../components/LiveBusTracker';
+import Footer from '../components/Footer';
 
 function BookingPage() {
-  const busId = 'example-bus-id';
+  const busId = null;
   return (
     <>
       <Header />
     <div>
         <BookingForm></BookingForm>
         <AvailableBus/>
-        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-          <LiveBusTracker busId={busId} />
+        
+
+        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' , backgroundColor: '#00000041'}}>
+        {busId ? ( <LiveBusTracker busId={busId} /> ) : <h4>Select a Bus</h4>}
         </div>
     </div>
+    <Footer/>
     </>
   )
 }
