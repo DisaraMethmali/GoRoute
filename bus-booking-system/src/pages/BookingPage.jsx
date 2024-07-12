e79f41dceffd4ac7ffdfaf92996286155f8bbd56
it22591302 <sandeepkahawatta9@gmail.com>
2024-07-12 17:07:48 +0530

    implement live locatio tracker to display bus route

--- bus-booking-system/src/pages/BookingPage.jsx
+++ bus-booking-system/src/pages/BookingPage.jsx
@@ -2,14 +2,19 @@ import React from 'react'
 import BookingForm from '../components/BookingForm'
 import Header from '../components/Header'
 import AvailableBus from '../components/AvailableBus'
+import LiveBusTracker from '../components/LiveBusTracker';
 
 function BookingPage() {
+  const busId = 'example-bus-id';
   return (
     <>
       <Header />
     <div>
         <BookingForm></BookingForm>
         <AvailableBus/>
+        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+          <LiveBusTracker busId={busId} />
+        </div>
     </div>
     </>
   )
