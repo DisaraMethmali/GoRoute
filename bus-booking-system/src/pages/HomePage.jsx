e611eab48de0d7e47a65576df9b2869ee6c795e6
Disara Methmali <it22080394@my.sliit.lk>
2024-07-12 15:58:50 +0530

    done

--- bus-booking-system/src/pages/HomePage.jsx
+++ bus-booking-system/src/pages/HomePage.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { Box, Container } from '@mui/material';
+import { Box, Container ,Typography,Rating } from '@mui/material';
 import Header from '../components/Header';
 import SearchBar from '../components/Search';
 import img from '../assets/img.png';
 import imgc from '../assets/imgc.png';
+import propic from '../assets/propic.png';
 const HomePage = () => {
   return (
     <>
@@ -23,9 +24,9 @@ const HomePage = () => {
                 mx: 'auto',
               }}
             />
-            <SearchBar/>
+            <SearchBar />
           </Box>
-          <Box sx={{ mt: 4, textAlign: 'left', position: 'relative' }}>
+          <Box sx={{ mt: 4, position: 'relative', display: 'flex', justifyContent: 'space-between' }}>
             <Box
               component="img"
               src={imgc}
@@ -37,11 +38,347 @@ const HomePage = () => {
                 display: 'block',
                 mx: 'auto',
                 position: 'absolute',
-                top: 0,
-                left: 0,
+                left: '50px',
+                top: '0px',
               }}
             />
+            <Typography
+              variant="body1"
+              sx={{
+                position: 'absolute',
+                top: '0',
+                right: '0',
+              fontSize:'32px',
+                color: '#727272',
+                fontFamily: 'Poppins',
+                fontWeight: 'bold',
+                zIndex: 10, // Ensure text appears above the image
+                padding: '8px',
+                borderRadius: '8px',
+              }}
+            >
+                 Explore Your Transport Media
+            </Typography>
+            <Typography
+              variant="body1"
+              sx={{
+                position: 'absolute',
+                top: '60px',
+                right: '-90px',
+              fontSize:'20px',
+                color: '#727272',
+                fontFamily: 'Poppins',
+                zIndex: 10, // Ensure text appears above the image
+                padding: '8px',
+                maxWidth: '600px',
+                textAlign: 'justify',
+                
+              }}
+            >
+                Lorem ipsum dolor sit amet consectetur.
+                 Faucibus non aliquet felis egestas. Egestas arcu eu eros mauris faucibus condimentum ac in.
+                 Lorem ipsum dolor sit amet consectetur.
+                 Faucibus non aliquet felis egestas. Egestas arcu eu eros mauris faucibus condimentum ac in.
+                 Lorem ipsum dolor sit amet consectetur.
+                 Faucibus non aliquet felis egestas. Egestas arcu eu eros mauris faucibus condimentum ac in.
+            </Typography>
+            <Typography
+              variant="body1"
+              sx={{
+                position: 'absolute',
+                top: '300px',
+                right: '390px',
+              fontSize:'22px',
+                color: '#727272',
+                fontFamily: 'Poppins',
+                fontWeight: 'bold',
+                zIndex: 10, // Ensure text appears above the image
+                padding: '8px',
+                borderRadius: '8px',
+              }}
+            >
+                See More 
+            </Typography>
+            <Box
+              component="img"
+              src={imgc}
+              alt="Descriptive Alt Text"
+              sx={{
+                width: '500px',
+                height: '400px',
+                borderRadius: '50px',
+                display: 'block',
+                mx: 'auto',
+                position: 'absolute',
+                right: '-100px',
+                top: '400px',
+              }}
+            />
+            <Typography
+              variant="body1"
+              sx={{
+                position: 'absolute',
+                top: '430px',
+                left: '50px',
+              fontSize:'32px',
+                color: '#727272',
+                fontFamily: 'Poppins',
+                fontWeight: 'bold',
+                zIndex: 10, // Ensure text appears above the image
+                padding: '8px',
+                borderRadius: '8px',
+              }}
+            >
+                 Live Tracking and Navigations
+            </Typography>
+            <Typography
+              variant="body1"
+              sx={{
+                position: 'absolute',
+                top: '500px',
+                left: '50px',
+              fontSize:'20px',
+                color: '#727272',
+                fontFamily: 'Poppins',
+                zIndex: 10, // Ensure text appears above the image
+                padding: '8px',
+                maxWidth: '600px',
+                textAlign: 'justify',
+                
+              }}
+            >
+                Lorem ipsum dolor sit amet consectetur.
+                 Faucibus non aliquet felis egestas. Egestas arcu eu eros mauris faucibus condimentum ac in.
+                 Lorem ipsum dolor sit amet consectetur.
+                 Faucibus non aliquet felis egestas. Egestas arcu eu eros mauris faucibus condimentum ac in.
+                 Lorem ipsum dolor sit amet consectetur.
+                 Faucibus non aliquet felis egestas. Egestas arcu eu eros mauris faucibus condimentum ac in.
+            </Typography>
+            <Typography
+              variant="body1"
+              sx={{
+                position: 'absolute',
+                top: '740px',
+                left: '50px',
+              fontSize:'22px',
+                color: '#727272',
+                fontFamily: 'Poppins',
+                fontWeight: 'bold',
+                zIndex: 10, // Ensure text appears above the image
+                padding: '8px',
+                borderRadius: '8px',
+              }}
+            >
+                See More 
+            </Typography>
+            <Box
+              component="img"
+              src={imgc}
+              alt="Descriptive Alt Text"
+              sx={{
+                width: '500px',
+                height: '400px',
+                borderRadius: '50px',
+                display: 'block',
+                mx: 'auto',
+                position: 'absolute',
+                left: '50px',
+                top: '820px',
+              }}
+            />
+            <Typography
+              variant="body1"
+              sx={{
+                position: 'absolute',
+                top: '830px',
+                right: '0',
+              fontSize:'32px',
+                color: '#727272',
+                fontFamily: 'Poppins',
+                fontWeight: 'bold',
+                zIndex: 10, // Ensure text appears above the image
+                padding: '8px',
+                borderRadius: '8px',
+              }}
+            >
+                 Pay Your Tickets More Easily
+            </Typography>
+            <Typography
+              variant="body1"
+              sx={{
+                position: 'absolute',
+                top: '890px',
+                right: '-90px',
+              fontSize:'20px',
+                color: '#727272',
+                fontFamily: 'Poppins',
+                zIndex: 10, // Ensure text appears above the image
+                padding: '8px',
+                maxWidth: '600px',
+                textAlign: 'justify',
+                
+              }}
+            >
+                Lorem ipsum dolor sit amet consectetur.
+                 Faucibus non aliquet felis egestas. Egestas arcu eu eros mauris faucibus condimentum ac in.
+                 Lorem ipsum dolor sit amet consectetur.
+                 Faucibus non aliquet felis egestas. Egestas arcu eu eros mauris faucibus condimentum ac in.
+                 Lorem ipsum dolor sit amet consectetur.
+                 Faucibus non aliquet felis egestas. Egestas arcu eu eros mauris faucibus condimentum ac in.
+            </Typography>
+            <Typography
+              variant="body1"
+              sx={{
+                position: 'absolute',
+                top: '1130px',
+                right: '390px',
+              fontSize:'22px',
+                color: '#727272',
+                fontFamily: 'Poppins',
+                fontWeight: 'bold',
+                zIndex: 10, // Ensure text appears above the image
+                padding: '8px',
+                borderRadius: '8px',
+              }}
+            >
+                See More 
+            </Typography>
           </Box>
+          <Typography variant="body1" sx={{ position: 'absolute',
+                top: '2050px',
+                left: '50px',
+              fontSize:'32px',
+                color: '#727272',
+                fontFamily: 'Poppins',
+                fontWeight: 'bold',
+                zIndex: 10, // Ensure text appears above the image
+                padding: '8px',
+                borderRadius: '8px',}}>
+               Feedbacks
+              </Typography>
+              <Box
+              component="img"
+              src={propic}
+              alt="Profile Picture"
+              sx={{
+                top:'2100px',
+                left: '240px',
+                width: '178px',
+                height: '178px',
+                borderRadius: '50%',
+                position: 'absolute',
+                bottom: '-60px', // Positioning the profile picture below the box
+                zIndex: 20, // Ensure the profile picture is above the box
+              }}
+            />
+          
+          <Box
+              width={500}
+              height={430}
+              my={4}
+              p={2}
+              sx={{
+                backgroundColor: '#D9D9D9',
+                borderRadius: '50px',
+                position: 'absolute',
+                bottom: '0',
+                top:'2150px',
+                left: '80px',
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                zIndex: 10, // Ensure the box is above other content
+              }}
+            >
+               <Typography
+              variant="body1"
+              sx={{
+                position: 'absolute',
+                top: '90px',
+                right: '20px',
+              fontSize:'20px',
+                color: '#727272',
+                fontFamily: 'Poppins',
+                zIndex: 10, // Ensure text appears above the image
+                padding: '8px',
+                maxWidth: '450px',
+                textAlign: 'justify',
+                
+              }}
+            >
+                Lorem ipsum dolor sit amet consectetur.
+                 Faucibus non aliquet felis egestas. Egestas arcu eu eros mauris faucibus condimentum ac in.
+                 Lorem ipsum dolor sit amet consectetur.
+                 Faucibus non aliquet felis egestas. Egestas arcu eu eros mauris faucibus condimentum ac in.
+                
+            </Typography>
+            <Box sx={{ position: 'absolute', top: '350px', left: '180px' }}>
+           
+            <Rating name="read-only" value={5} readOnly />
+          </Box>
+            </Box>
+            <Box
+              component="img"
+              src={propic}
+              alt="Profile Picture"
+              sx={{
+                top:'2100px',
+                right: '255px',
+                width: '178px',
+                height: '178px',
+                borderRadius: '50%',
+                position: 'absolute',
+                bottom: '-60px', // Positioning the profile picture below the box
+                zIndex: 20, // Ensure the profile picture is above the box
+              }}
+            />
+          
+          <Box
+              width={500}
+              height={430}
+              my={4}
+              p={2}
+              sx={{
+                backgroundColor: '#D9D9D9',
+                borderRadius: '50px',
+                position: 'absolute',
+                bottom: '0',
+                top:'2150px',
+                right: '100px',
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                zIndex: 10, // Ensure the box is above other content
+              }}
+            > <Typography
+            variant="body1"
+            sx={{
+              position: 'absolute',
+              top: '90px',
+             left: '20px',
+            fontSize:'20px',
+              color: '#727272',
+              fontFamily: 'Poppins',
+              zIndex: 10, // Ensure text appears above the image
+              padding: '8px',
+              maxWidth: '450px',
+              textAlign: 'justify',
+              
+            }}
+          >
+              Lorem ipsum dolor sit amet consectetur.
+               Faucibus non aliquet felis egestas. Egestas arcu eu eros mauris faucibus condimentum ac in.
+               Lorem ipsum dolor sit amet consectetur.
+               Faucibus non aliquet felis egestas. Egestas arcu eu eros mauris faucibus condimentum ac in.
+              
+          </Typography>
+          <Box sx={{ position: 'absolute', top: '350px', left: '200px' }}>
+           
+           <Rating name="read-only" value={4} readOnly />
+         </Box>
+              </Box>
+     
+   
         </Container>
       </Box>
     </>
