cf9c1e033911f4f2f4342e946bb2efa0cf2e8e1c
Disara Methmali <it22080394@my.sliit.lk>
2024-07-12 12:55:12 +0530

    done3

--- bus-booking-system/src/App.jsx
+++ bus-booking-system/src/App.jsx
@@ -3,12 +3,15 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import CssBaseline from '@mui/material/CssBaseline';
 
+import createTheme from "@mui/material/styles/createTheme";
+const theme = createTheme();
+
 function App() {
   return (
     <Router>
       <CssBaseline />
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route exact path="/" element={<HomePage/>} />
       </Routes>
     </Router>
   );
--- bus-booking-system/src/pages/HomePage.jsx
+++ bus-booking-system/src/pages/HomePage.jsx
@@ -1,17 +1,50 @@
 import React from 'react';
-import BookingForm from '../components/BookingForm';
-import { Container, Box, Typography } from '@mui/material';
-
+import { Box, Container } from '@mui/material';
+import Header from '../components/Header';
+import SearchBar from '../components/Search';
+import img from '../assets/img.png';
+import imgc from '../assets/imgc.png';
 const HomePage = () => {
   return (
-    <Container>
-      <Box sx={{ mt: 4, textAlign: 'center' }}>
-        <Typography variant="h3" component="h1" sx={{ mb: 2 }}>
-          Get Your Ticket Online, Easy and Safely
-        </Typography>
-        <BookingForm />
+    <>
+      <Header />
+      <Box sx={{ bgcolor: '#fefefe', pt: 8 }}>
+        <Container maxWidth="lg">
+          <Box sx={{ mt: 4, textAlign: 'center', position: 'relative' }}>
+            <Box
+              component="img"
+              src={img}
+              alt="Descriptive Alt Text"
+              sx={{
+                width: '1300px',
+                height: '678px',
+                borderRadius: '50px',
+                display: 'block',
+                mx: 'auto',
+              }}
+            />
+            <SearchBar/>
+          </Box>
+          <Box sx={{ mt: 4, textAlign: 'left', position: 'relative' }}>
+            <Box
+              component="img"
+              src={imgc}
+              alt="Descriptive Alt Text"
+              sx={{
+                width: '500px',
+                height: '400px',
+                borderRadius: '50px',
+                display: 'block',
+                mx: 'auto',
+                position: 'absolute',
+                top: 0,
+                left: 0,
+              }}
+            />
+          </Box>
+        </Container>
       </Box>
-    </Container>
+    </>
   );
 };
 
